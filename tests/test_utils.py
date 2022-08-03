b627cedb632832fe89491fbbaa3c6bb3008fc9ea
Fran Bartolic <fb90@st-andrews.ac.uk>
2022-08-03 15:22:17 +0200

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,6 +11,7 @@ from caustics.utils import (
     trapz_zero_avoiding,
 )
 
+config.update("jax_platform_name", "cpu")
 config.update("jax_enable_x64", True)
 
 
@@ -29,22 +30,13 @@ def test_max_zero_avoiding():
 
 
 def test_trapz_zero_avoiding():
-    x1 = jnp.linspace(0, 1, 13)
-    x2 = jnp.linspace(0, 0.3, 10)
+    x = jnp.linspace(0, 1, 13)
+    y = jnp.cos(x)
+    I = jnp.trapz(y, x)
 
-    y1 = jnp.cos(x1)
-    y2 = jnp.cos(x2)
+    x = jnp.append(x, jnp.zeros(7))
+    y = jnp.append(y, jnp.zeros(7))
+    tidx = 12
+    res = trapz_zero_avoiding(y, x, tidx)
 
-    I1 = jnp.trapz(y1, x1)
-    I2 = jnp.trapz(y2, x2)
-
-    x1 = jnp.append(x1, jnp.zeros(7))
-    x2 = jnp.append(x2, jnp.zeros(10))
-    y1 = jnp.append(y1, jnp.zeros(7))
-    y2 = jnp.append(y2, jnp.zeros(10))
-    x = jnp.stack([x1, x2])
-    y = jnp.stack([y1, y2])
-    tail_idcs = jnp.array([12, 9])
-    res = trapz_zero_avoiding(y, x, tail_idcs, axis=1)
-
-    np.testing.assert_array_equal(res, jnp.array([I1, I2]))
+    np.testing.assert_array_equal(res, I)
